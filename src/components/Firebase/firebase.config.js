735c1cc1842e6aaca41c9ca739f0617df2f0d8f7
Abrarulctg <jubairctg99@gmail.com>
2024-04-30 23:32:15 +0600

    Added .env file

--- src/components/Firebase/firebase.config.js
+++ src/components/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBVw2Kl-R-MztD3yuMF-SXyJxUSrOPZUR4",
-    authDomain: "abrar-tourism.firebaseapp.com",
-    projectId: "abrar-tourism",
-    storageBucket: "abrar-tourism.appspot.com",
-    messagingSenderId: "757540209650",
-    appId: "1:757540209650:web:1a346c298358ee7d23616b"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
